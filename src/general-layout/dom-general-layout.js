6bd83772fba6f60937c1d3d10188780c07641719
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-01-28 22:26:48 -0500

    Refactor code to use the module pattern

--- src/general-layout/dom-general-layout.js
+++ src/general-layout/dom-general-layout.js
@@ -3,9 +3,9 @@ import PubSub from 'pubsub-js';
 
 export const GENERAL_LAYOUT = 'load general layout';
 
-function createGeneralLayout(){
-    const body = document.querySelector('body');
+const body = document.querySelector('body');
 
+const header = (function(){
     function createHeader(){
         const headerElement = document.createElement('header');
         headerElement.classList.add('header');
@@ -13,7 +13,11 @@ function createGeneralLayout(){
         body.appendChild(headerElement);
     }
 
-    function addChildrenToMain(domElement){
+    return { createHeader };
+})();
+
+const main = (function(){
+    function addChildrenToMainSection(domElement){
         const sideBar = document.createElement('div');
         const content = document.createElement('div');
 
@@ -30,35 +34,117 @@ function createGeneralLayout(){
 
         body.appendChild(mainSectionDiv);
 
-        addChildrenToMain(mainSectionDiv);
+        addChildrenToMainSection(mainSectionDiv);
     }
 
-    function addContentToSideBar(){
+    return { createMainSection };
+})();
+
+const sideBar = (function(){
+    function createFilterOption(parentElement, filterType){
+        const filterOption = document.createElement('div');
+        const filterOptionText = document.createElement('div');
+        const taskCount = document.createElement('div');
+    
+        filterOption.classList.add('filter-option');
+        filterOptionText.textContent = filterType;
+    
+        taskCount.classList.add('task-count');
+    
+        filterOption.appendChild(filterOptionText);
+        filterOption.appendChild(taskCount);
+    
+        parentElement.appendChild(filterOption);
+    }
+    
+    function createSideBarTextDiv(parentElement, text, className){
+        const div = document.createElement('div');
+        
+        if(text) div.textContent = text;
+    
+        if(className) div.classList.add(`${className}`);
+    
+        parentElement.appendChild(div);
+    }
+    
+    function createSideBarFilterOptions(){
+        const sideBarFilterOptions = document.createElement('div');
+        sideBarFilterOptions.classList.add('side-bar-filter-options');
+
+        createFilterOption(sideBarFilterOptions, 'INBOX');
+        createFilterOption(sideBarFilterOptions, 'TODAY');
+        createFilterOption(sideBarFilterOptions, 'UPCOMING');
+        createFilterOption(sideBarFilterOptions, 'ANYTIME');
+
+        return sideBarFilterOptions;
+    }
+
+    function createProjects(){
+        const projectsContainer = document.createElement('div');
+        const projectsList = document.createElement('div');
+
+        projectsContainer.classList.add('projects-container');
+        projectsList.classList.add('projects-list');
+
+        createSideBarTextDiv(projectsContainer, 'PROJECTS', 'projects-title');
+
+        projectsContainer.appendChild(projectsList);
+
+        createSideBarTextDiv(projectsList, 'FILLER 1');
+        createSideBarTextDiv(projectsList, 'FILLER 2');
+        createSideBarTextDiv(projectsList, 'FILLER 3');
+
+        return projectsContainer;
+    }
+
+    function createAddProjectBtn(){
+        const btnContainer = document.createElement('div');
+        const addProjectBtn = document.createElement('button');
+
+        btnContainer.classList.add('project-btn-container');
+        addProjectBtn.classList.add('add-project-btn');
+
+        addProjectBtn.textContent = '+ ADD PROJECT';
+        
+        btnContainer.appendChild(addProjectBtn);
+
+        return btnContainer;
+    }
+
+    function createSideBarDivider(){
+        const dividerContainer = document.createElement('div');
+        const divider = document.createElement('div');
+
+        dividerContainer.classList.add('divider-container');
+        divider.classList.add('side-bar-divider');
+
+        dividerContainer.appendChild(divider);
+
+        return dividerContainer;
+    }
+
+    function createSideBarSection(){
         const sideBar = document.querySelector('.side-bar');
-
         const sideBarContentContainer = document.createElement('div');
+
         sideBarContentContainer.classList.add('side-bar-content-container');
-
+        
         sideBar.appendChild(sideBarContentContainer);
-
-        addDiv(sideBarContentContainer, 'INBOX');
-        addDiv(sideBarContentContainer, 'TODAY');
-        addDiv(sideBarContentContainer, 'UPCOMING');
-        addDiv(sideBarContentContainer, 'ANYTIME');
-
+        sideBarContentContainer.appendChild(createSideBarFilterOptions());
+        sideBarContentContainer.appendChild(createSideBarDivider());
+        sideBarContentContainer.appendChild(createProjects());
+        sideBarContentContainer.appendChild(createSideBarDivider());
+        sideBarContentContainer.appendChild(createAddProjectBtn());
     }
 
-    createHeader();
-    createMainSection();
-    addContentToSideBar();
-}
+    return { createSideBarSection };
+})();
 
-function addDiv(parentElement, text){
-    const div = document.createElement('div');
 
-    div.textContent = text;
-
-    parentElement.appendChild(div);
+function createGeneralLayout(){
+    header.createHeader();
+    main.createMainSection();
+    sideBar.createSideBarSection();
 }
 
 PubSub.subscribe(GENERAL_LAYOUT, createGeneralLayout);
