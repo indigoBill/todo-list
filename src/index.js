a0d03560cbf7a7628f6e7b1649cb085b83212aac
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-01 10:36:18 -0500

    Publish to a new topic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { GENERAL_LAYOUT } from './barrel.js';
+import { GENERAL_LAYOUT, EVENT_LISTENERS } from './barrel.js';
 import PubSub from 'pubsub-js';
 
 document.addEventListener('DOMContentLoaded', () => {
     PubSub.publish(GENERAL_LAYOUT);
+    PubSub.publish(EVENT_LISTENERS);
 });
-
 
 
