3209b89d5f7e1fe4bdf3d6a9d5bbb4f4c701f7ff
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-03 21:16:01 -0500

    Create add task button

--- src/content/content_section.js
+++ src/content/content_section.js
@@ -6,12 +6,68 @@ const content = function(){
 
     const content = document.querySelector('.content');
 
-    const rect = document.createElement('div');
+    function createAddTaskButton(){
+        const taskAdderContainer = document.createElement('div');
+        const taskAdderBtn = document.createElement('button');
 
-    rect.classList.add('rect');
+        taskAdderContainer.classList.add('add-task-btn-container');
+        taskAdderBtn.classList.add('add-task-btn');
 
-    content.appendChild(rect);
+        taskAdderBtn.textContent = '+ ADD TASK';
 
+        taskAdderContainer.appendChild(taskAdderBtn);
+        content.appendChild(taskAdderContainer);
+    }
+
+    function createTitleInput(){
+        const titleContainer = document.createElement('div');
+        const titleLabel = document.createElement('label');
+        const title = document.createElement('input');
+
+        titleLabel.textContent = 'TITLE:';
+
+        titleLabel.setAttribute('for', 'title');
+        title.setAttribute('id', 'title');
+
+        titleContainer.appendChild(titleLabel);
+        titleContainer.appendChild(title);
+
+        return titleContainer;
+    }
+
+    function createDescriptionInput(){
+        const descriptionContainer = document.createElement('div');
+        const descriptionLabel = document.createElement('label');
+        const description = document.createElement('input');
+
+        descriptionLabel.textContent = 'DESCRIPTION:';
+
+        descriptionLabel.setAttribute('for', 'description');
+        description.setAttribute('id', 'description');
+
+        descriptionContainer.appendChild(descriptionLabel);
+        descriptionContainer.appendChild(description);
+
+        return descriptionContainer;
+    }
+
+    function createDueDateCalendar(){
+
+    }
+
+    function createTask(){
+        const taskContainer = document.createElement('div');
+        
+        taskContainer.classList.add('task-container');
+
+        taskContainer.appendChild(createTitleInput());
+        taskContainer.appendChild(createDescriptionInput());
+
+        content.appendChild(taskContainer);
+    }
+
+    createAddTaskButton();
+    createTask();
 };
 
 PubSub.subscribe(GENERAL_LAYOUT, content);
