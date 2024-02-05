d442726ae3563574790fb68deee88bcab2d9a8fd
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-05 10:51:31 -0500

    Update comments

--- src/barrel.js
+++ src/barrel.js
@@ -1,4 +1,4 @@
-//INCLUDES: EVENT_LISTENERS, LISTENERS
+//INCLUDES: EVENT_LISTENERS
 export * from './header/header_logic.js';
 
 //INCLUDES: GENERAL_LAYOUT
@@ -7,6 +7,9 @@ export * from './header/header_section.js';
 export * from './main/main_section.js';
 
 export * from './sideBar/sideBar_section.js';
+
+//INCLUDES: CREATE_TASK_ADDER, CHECK_TASK_INPUTS, TASK_ADDER_EVENT_LISTENER
+export * from './content/content_logic.js';
 
 export * from './content/content_section.js';
 
