14fb29099e2cf97b6ceb7abad97eb5ffb1313b8c
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-07 10:39:55 -0500

    Add event listeners to icons in added tasks

--- src/content/content_logic.js
+++ src/content/content_logic.js
@@ -4,6 +4,8 @@ import { EVENT_LISTENERS } from '../barrel.js';
 export const SHOW_TASK_ADDER = 'display task adder';
 export const CHECK_TASK_INPUTS = 'check that the required inputs have value';
 export const TASK_ADDER_EVENT_LISTENER = 'add event listener to buttons in task adder container';
+export const EXTEND_TASK = 'extend added task to view description and priority level';
+export const ADDED_TASK_EVENT_LISTENER = 'add event listener to icons in added tasks';
 
 function createTaskAdderBtnEventListener(){
     const addTaskBtn = document.querySelector('.add-task-btn');
@@ -29,6 +31,20 @@ function createCancelBtnEventListener(){
     });
 }
 
+function createExtendTaskIconEventListener(){
+    const upDownIcon = document.querySelectorAll('.up-down-icon');
+
+    //NODELIST INDEX WILL BE THE SAME SO FIGURE OUT A WAY TO USE THAT 
+    //TO DETERMINE WHICH DISPLAY TO EXTEND
+
+    upDownIcon.forEach((icon, index) => {
+        icon.addEventListener('click', () => {
+            console.log(index);
+            PubSub.publish(EXTEND_TASK, index);
+        });
+    });
+}
+
 function loadEventListeners(){
     createTaskAdderBtnEventListener();
 }
@@ -38,5 +54,11 @@ function loadTaskAdderEventListeners(){
     createCancelBtnEventListener();
 }
 
+function loadAddedTaskEventListener(){
+    createExtendTaskIconEventListener();
+}
+
+
 PubSub.subscribe(EVENT_LISTENERS, loadEventListeners);
 PubSub.subscribe(TASK_ADDER_EVENT_LISTENER, loadTaskAdderEventListeners);
+PubSub.subscribe(ADDED_TASK_EVENT_LISTENER, loadAddedTaskEventListener);
