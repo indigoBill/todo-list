31b8899238eccde2093a1cbe2ddaa1a67888cf65
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-21 10:45:52 -0500

    Add an event listener to the search bar

--- src/header/header_logic.js
+++ src/header/header_logic.js
@@ -1,18 +1,46 @@
 import PubSub from 'pubsub-js';
+import { DEFAULT_CURRENT_TAB, SEARCH_TASKS } from '../barrel.js';
 
-export const EVENT_LISTENERS = 'load event listeners';
-export const LISTENER = 'top level header section listener topic';
+export const EVENT_LISTENERS = 'add event listeners on page load';
+export const SIDEBAR_DISPLAY = 'display sidebar';
 
 function createMenuBtnEventListener(){
     const menuBtn = document.querySelector('.menu-btn');
 
-    menuBtn.addEventListener('click', () => {
-        PubSub.publish(LISTENER.main);
+    menuBtn.addEventListener('click', () => {        
+        PubSub.publish(SIDEBAR_DISPLAY);
     });
-};
+}
+
+function createHomeBtnEventListener(){
+    const homeBtn = document.querySelector('.home-btn');
+
+    homeBtn.addEventListener('click', () => {
+        PubSub.publish(DEFAULT_CURRENT_TAB);
+    });
+}
+
+function createSearchBtnEventListener(){
+    const searchBtn = document.querySelector('.search-btn');
+
+    searchBtn.addEventListener('click', () => {
+        document.querySelector('#search').focus();
+    });
+}
+
+function createSearchBarEventListener(){
+    const searchBar = document.querySelector('#search');
+
+    searchBar.addEventListener('keydown', () => {
+        PubSub.publish(SEARCH_TASKS);
+    });
+}
 
 function loadEventListeners(){
     createMenuBtnEventListener();
+    createHomeBtnEventListener();
+    createSearchBtnEventListener();
+    createSearchBarEventListener();
 }
 
 PubSub.subscribe(EVENT_LISTENERS, loadEventListeners);
