46d3664a701a0c75a92451be44a6da8fc2aa6f5b
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-21 10:46:30 -0500

    Remove plus button

--- src/header/header_section.js
+++ src/header/header_section.js
@@ -5,78 +5,75 @@ export const GENERAL_LAYOUT = 'load general layout';
 
 const body = document.querySelector('body');
 
-const header = (function(){
-    function createHeaderTitle(){
-        const headerTitleContainer = document.createElement('div');
-        const headerTitle = document.createElement('div');
+function createHeaderTitle(){
+    const headerTitleContainer = document.createElement('div');
+    const headerTitle = document.createElement('div');
 
-        headerTitleContainer.classList.add('header-title-container');
-        headerTitle.classList.add('header-title');
+    headerTitleContainer.classList.add('header-title-container');
+    headerTitle.classList.add('header-title');
 
-        headerTitle.textContent = 'Todo List';
+    headerTitle.textContent = 'Todo List';
 
-        headerTitleContainer.appendChild(headerTitle);
+    headerTitleContainer.appendChild(headerTitle);
 
-        return headerTitleContainer;
-    }
+    return headerTitleContainer;
+}
 
-    function createIconButton(iconClass){
-        const iconBtnContainer = document.createElement('div');
-        const iconBtn = document.createElement('button');
+function createIconButton(iconClass){
+    const iconBtnContainer = document.createElement('div');
+    const iconBtn = document.createElement('button');
 
-        iconBtnContainer.classList.add('icon-btn-container');
-        iconBtn.classList.add('icon-btn');
-        iconBtn.classList.add(iconClass);
+    iconBtnContainer.classList.add('icon-btn-container');
+    iconBtn.classList.add('icon-btn');
+    iconBtn.classList.add(iconClass);
 
-        iconBtnContainer.appendChild(iconBtn);
+    iconBtnContainer.appendChild(iconBtn);
 
-        return iconBtnContainer;
-    }
+    return iconBtnContainer;
+}
 
-    function createSearchInput(){
-        const inputContainer = document.createElement('div');
-        const label = document.createElement('label');
-        const input = document.createElement('input');
+function createSearchInput(){
+    const inputContainer = document.createElement('div');
+    const label = document.createElement('label');
+    const input = document.createElement('input');
 
-        label.setAttribute('for', 'search');
-        input.setAttribute('type', 'text');
-        input.setAttribute('name', 'search');
-        input.setAttribute('id', 'search');
+    label.textContent = 'SEARCH';
 
-        inputContainer.classList.add('input-container');
-        label.classList.add('visually-hidden');
+    label.setAttribute('for', 'search');
+    input.setAttribute('type', 'text');
+    input.setAttribute('name', 'search');
+    input.setAttribute('id', 'search');
 
-        inputContainer.appendChild(createIconButton('search-btn'));
-        inputContainer.appendChild(label);
-        inputContainer.appendChild(input);
+    inputContainer.classList.add('input-container');
+    label.classList.add('visually-hidden');
 
-        return inputContainer;
-    }
-    
-    function createHeaderSection(){
-        const headerElement = document.createElement('header');
-        const headerContentContainer = document.createElement('div');
-        const headerTopSection = document.createElement('div');
-        const headerBottomSection = document.createElement('div');
+    inputContainer.appendChild(createIconButton('search-btn'));
+    inputContainer.appendChild(label);
+    inputContainer.appendChild(input);
 
-        headerElement.classList.add('header');
-        headerContentContainer.classList.add('header-content-container');
-        headerTopSection.classList.add('header-top-section');
-        headerBottomSection.classList.add('header-bottom-section');
+    return inputContainer;
+}
 
-        body.appendChild(headerElement);
-        headerElement.appendChild(headerContentContainer);
-        headerContentContainer.appendChild(headerTopSection);
-        headerContentContainer.appendChild(headerBottomSection);
+function createHeaderSection(){
+    const headerElement = document.createElement('header');
+    const headerContentContainer = document.createElement('div');
+    const headerTopSection = document.createElement('div');
+    const headerBottomSection = document.createElement('div');
 
-        headerTopSection.appendChild(createHeaderTitle());
-        headerTopSection.appendChild(createSearchInput());
-        headerBottomSection.appendChild(createIconButton('menu-btn'));
-        headerBottomSection.appendChild(createIconButton('home-btn'));
-        headerBottomSection.appendChild(createIconButton('plus-btn'));
-    }
+    headerElement.classList.add('header');
+    headerContentContainer.classList.add('header-content-container');
+    headerTopSection.classList.add('header-top-section');
+    headerBottomSection.classList.add('header-bottom-section');
 
-    return { createHeaderSection };
-})();
+    body.appendChild(headerElement);
+    headerElement.appendChild(headerContentContainer);
+    headerContentContainer.appendChild(headerTopSection);
+    headerContentContainer.appendChild(headerBottomSection);
 
-PubSub.subscribe(GENERAL_LAYOUT, header.createHeaderSection);
+    headerTopSection.appendChild(createHeaderTitle());
+    headerTopSection.appendChild(createSearchInput());
+    headerBottomSection.appendChild(createIconButton('menu-btn'));
+    headerBottomSection.appendChild(createIconButton('home-btn'));
+}
+
+PubSub.subscribe(GENERAL_LAYOUT, createHeaderSection);
