97888201ea26b7222c932fd19e74e76c96f4dfe8
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-23 11:33:59 -0500

    Publish to topics that affect the localStorage

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-import { GENERAL_LAYOUT, EVENT_LISTENERS } from './barrel.js';
+import { GENERAL_LAYOUT, EVENT_LISTENERS, LOAD_STORAGE_TASKS, LOAD_STORAGE_PROJECTS } from './barrel.js';
 import PubSub from 'pubsub-js';
 
 document.addEventListener('DOMContentLoaded', () => {
     PubSub.publish(GENERAL_LAYOUT);
     PubSub.publish(EVENT_LISTENERS);
+
+    if(localStorage.getItem('allTasks')){
+        PubSub.publish(LOAD_STORAGE_TASKS);
+    }
+
+    if(localStorage.getItem('allProjects')){
+        PubSub.publish(LOAD_STORAGE_PROJECTS);
+    }
 });
 
-// function populateStorage(){
-//     const allProjects = document.querySelectorAll('.project');
-//     const allTasks = document.querySelectorAll('.task-container');
-//     let projectCounter = 1;
-//     let taskCounter = 1;
 
-//     allProjects.forEach((project) => {
-//         let projectKey = 'project' + projectCounter;
-
-//         localStorage.setItem(projectKey, JSON.stringify(project));
-//     });
-
-//     allTasks.forEach((task) => {
-//         let taskKey = 'task' + taskCounter;
-
-//         localStorage.setItem(taskKey, JSON.stringify(task));
-//     });
-    
-// }
 
 
