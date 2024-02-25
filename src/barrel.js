60eba12aa6d32480c7c03f5417e722eb8781cdfe
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-02-25 14:51:46 -0500

    Update comments to track of PubSub topics origins

--- src/barrel.js
+++ src/barrel.js
@@ -1,4 +1,4 @@
-//INCLUDES: EVENT_LISTENERS
+//INCLUDES: EVENT_LISTENERS, SIDEBAR_DISPLAY
 export * from './header/header_logic.js';
 
 //INCLUDES: GENERAL_LAYOUT
@@ -8,11 +8,12 @@ export * from './main/main_section.js';
 
 export * from './sideBar/sideBar_section.js';
 
+//INCLUDES: TASK_COUNT, DEFAULT_CURRENT_TAB, LOAD_STORAGE_PROJECTS
 export * from './sideBar/sideBar_logic.js';
 
 export * from './content/content_section.js';
 
-//INCLUDES: TASK_ADDER_EVENT_LISTENER
+//INCLUDES: TASK_ADDER_EVENT_LISTENER, TASK_EVENT_LISTENER, PROJECT_DROPDOWN, PROJECT_ATRIBUTE, CURRENT_TAB, SEARCH_TASKS, LOAD_STORAGE_TASKS
 export * from './content/content_logic.js';
 
 
